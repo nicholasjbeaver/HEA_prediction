bc283df30901eb5516951ac92d7d6802591860f5
jim <home@jimbeaver.com>
2024-01-11 22:11:57 +0000

    Moved things around in prediction_client to make sure messages aren't overwritten

--- test_pub_sub.py
+++ test_pub_sub.py
@@ -21,10 +21,18 @@ logger.setLevel(logging.INFO)
 
 # TODO:
 
+@dataclass
+class input_message:    
+    alloy: str
+    crystal: str
+    do_relaxation: bool
+
+
+# main
 
 if __name__ == "__main__":
 
-    publish_topic = GOOGLE_PUBSUB_TOPIC
+    publish_topic = "test_topic"
 
     # create a dictionary that includes key, corpus_id and type
     data = {"key": "value"}
@@ -42,12 +50,46 @@ if __name__ == "__main__":
     logger.info(f"Creating a subscription for topic: {sub_topic_id}")
     subscription_id = pubsub.subscription(topic_id=sub_topic_id, subscription_id="testing_sub", **attributes)
 
-    loggier.debug(f'Created a subscription: {subscription_id}')
+    logger.debug(f'Created a subscription: {subscription_id}')
     pubsub.delete_when_done(name=subscription_id)
 
     # Publish the message
     logger.debug(f"Publish the message: {data} on topic {pub_topic_id}")
     pubsub.publish(data, topic_id=pub_topic_id, **attributes)
+
+    # make df of the csv for ease of use
+    df = pd.read_csv(filename)
+
+    # iterate over rows of input file and turn into json objects    
+    for index, row in df.iterrows():
+
+        # make json message out of a row made into a dictionary
+        json_message = json.dumps(row.to_dict()).encode('utf-8')
+
+        '''
+        try:
+            json_object = json.loads(json_message)
+            logger.debug('good json')
+        except json.JSONDecodeError as e:
+            logger.debug('bad json')
+        '''
+        logger.debug(f"Publish the message: {json_message} on topic {pub_topic_id}")
+
+        # Define your data and attributes.  These are optional and only used for filtering messages.
+        attributes = {}
+        future = pubsub.publish(json_message, topic_id=pub_topic_id, **attributes)
+        result = future.result()
+
+        try:
+        # This will block until the message is published
+            message_id = future.result()
+            logger.debug(f"Message published with ID: {message_id}")
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
+
+
+
+
 
     # loop until receiving a signal to exit.
     try:
