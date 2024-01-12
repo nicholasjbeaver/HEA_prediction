9fead0e5e8c918dac5dddc0e84f251111d005f27
jim <home@jimbeaver.com>
2024-01-12 18:21:22 +0000

    add ability to specify local build or remote build

--- build/build_deploy_prediction.sh
+++ build/build_deploy_prediction.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 #
-# Build Corpus Keeper Docker image for ingestion server
+# Build and deploy Docker image for prediction_server
 #
 set -x  # verbose echo mode on so can see expansion of variables etc.
+
+# see if first parameter is "cloud_build".  It will indicate whether to use remote build
+if [ "$1" == "cloud_build" ]; then
+    CLOUD_BUILD="true"
+else
+    CLOUD_BUILD="false"
+fi
+
 
 # start from the corpuskeeper/rag/build directory
 CK_INGEST_DIR="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd ))"
@@ -14,13 +22,13 @@ echo $CK_INGEST_DIR
 cp "$CK_INGEST_DIR/build/Dockerfile" "$CK_DIR/Dockerfile"
 
 # shellcheck disable=SC1101
-gcloud builds submit "$CK_DIR" \
-  --tag "us-central1-docker.pkg.dev/gpt-funhouse/containers/${CKBASE}-ingest" \
-  --region us-central1
-
-
-# to test build locally:
-#docker build -t "${CKBASE}-ingest" -f "$CK_INGEST_DIR/Dockerfile" "$CK_DIR"
+if [ "$CLOUD_BUILD" == "true" ]; then
+  gcloud builds submit "$CK_DIR" \
+    --tag "us-central1-docker.pkg.dev/phase-predition/containers/${CKBASE}-ingest" \
+    --region us-central1
+else
+  # to test build locally:
+  docker build -t "${CKBASE}-ingest" -f "$CK_INGEST_DIR/Dockerfile" "$CK_DIR"
 
 BUILD_STATUS=$?
 
@@ -32,12 +40,16 @@ if ! [ $BUILD_STATUS -eq 0 ]; then
     exit 1
 fi
 
+# if we get here, build succeeded, but do not auto deploy for now
+echo "Build succeeded...not deploying."
+exit 0
+
 echo "Build succeeded...deploying."
 
 # always have one production server running, but can scale down to zero for dev
 if [ "$ENV" == "prod" ]; then MIN_INSTANCES=0; else MIN_INSTANCES=0; fi
 gcloud run deploy ${CKBASE}-ingest \
-  --image "us-central1-docker.pkg.dev/gpt-funhouse/containers/${CKBASE}-ingest" \
+  --image "us-central1-docker.pkg.dev/phase-prediction/containers/prediction_server" \
   --allow-unauthenticated \
   --region us-central1 \
   --labels "env=${ENV}" \
@@ -45,5 +57,5 @@ gcloud run deploy ${CKBASE}-ingest \
   --max-instances 10 \
   --timeout 600 \
   --memory 2Gi \
-  --set-env-vars "ENV=${ENV},GOOGLE_CLOUD_PROJECT=gpt-funhouse"
+  --set-env-vars "ENV=${ENV},GOOGLE_CLOUD_PROJECT=phase-prediction"
 
