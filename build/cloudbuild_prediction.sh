56aefa8b8aaf561ae77cd920f9a119da61b6f58a
jim <home@jimbeaver.com>
2024-01-17 23:37:37 +0000

    add cloudbuild by default

--- build/cloudbuild_prediction.sh
+++ build/cloudbuild_prediction.sh
@@ -16,29 +16,25 @@ else
 fi
 
 # Set the build directory so it is unique for each environment
-BUILD_DIR="HEA_prediction_${ENV}"
-echo "Building for environment: $ENV in directory: ${BUILD_DIR}"
-
+PROJECT_DIR="HEA_prediction_${ENV}"
+echo "Building for environment: $ENV in directory: ${PROJECT_DIR}"
 
 # Clone the repository from GitHub, create it if it doesn't exist
-if [ ! -d "$BUILD_DIR" ]; then
-    echo "Cloning repository"
-    git clone https://github.com/nicholasjbeaver/HEA_prediction "$BUILD_DIR"
+if [ ! -d "$PROJECT_DIR" ]; then
+    echo "Cloning repository into ${PROJECT_DIR}"
+    git clone https://github.com/nicholasjbeaver/HEA_prediction "$PROJECT_DIR"
 
-    cd "$BUILD_DIR"
+    cd "$PROJECT_DIR"
 
     # Checkout the main branch
     git checkout main
     git pull origin main
 else
-    echo "Repository already exists, will build with existing files"
+    echo "Repository already exists, will build with existing files in ${PROJECT_DIR}"
+    cd "$PROJECT_DIR"
 fi
 
-cd "$BUILD_DIR"
-
 # Submit the build with the environment value as a substitution
-# gcloud builds submit --region=us-central1 --config cloudbuild_ingest.yaml --substitutions=_ENV="$ENV" .
+gcloud builds submit --region=us-central1 --config "./build/cloudbuild_prediction.yaml" --substitutions=_ENV="$ENV" .
 
-# call build_deploy-ingest.sh script with environment value as parameter
-# source ./build_deploy_prediction.sh
 echo "Done"
