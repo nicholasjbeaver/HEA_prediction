d380404fe1a5e2b38f27958f860ee06d70482b51
jim <home@jimbeaver.com>
2024-01-17 20:20:26 +0000

    changing the directory structure

--- build/build_deploy_prediction.sh
+++ build/build_deploy_prediction.sh
@@ -11,27 +11,38 @@ else
     CLOUD_BUILD="false"
 fi
 
+# see what ENV we are building, check ENV variable
+if [ $ENV == "PROD" ]; then
+  ENV="PROD"
+else
+  ENV="TEST"
+fi
 
-# start from the corpuskeeper/rag/build directory
-CK_INGEST_DIR="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd ))"
-CK_DIR="$(dirname "$CK_INGEST_DIR")"
+# find out the name of the directory containing this script
+BUILD_DIR="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd ))"
+PROJECT_DIR="$(dirname "${BUILD_DIR}")"
 
 
-echo $CK_INGEST_DIR
-cp "$CK_INGEST_DIR/build/Dockerfile" "$CK_DIR/Dockerfile"
+echo $BUILD_DIR
+cp "${BUILD_DIR}/build/Dockerfile" "${PROJECT_DIR}/Dockerfile"
 
 # shellcheck disable=SC1101
 if [ "$CLOUD_BUILD" == "true" ]; then
-  gcloud builds submit "$CK_DIR" \
-    --tag "us-central1-docker.pkg.dev/phase-predition/containers/prediction-server-$CKBASE" \
+  gcloud builds submit "$PROJECT_DIR" \
+    --tag "us-central1-docker.pkg.dev/phase-predition/containers/prediction-server-${ENV}" \
     --region us-central1
 else
   # to test build locally:
-  docker build -t "${CKBASE}-ingest" -f "$CK_INGEST_DIR/Dockerfile" "$CK_DIR"
+  docker build -t "prediction-server-${ENV}" -f "./Dockerfile" "${PROJECT_DIR}"
+
+  # push docker image to cloud artifact registry
+
+
+fi
 
 BUILD_STATUS=$?
 
-rm -f "$CK_DIR/Dockerfile"
+rm -f "${PROJECT_DIR}/Dockerfile"
 
 # Check the exit status
 if ! [ $BUILD_STATUS -eq 0 ]; then
